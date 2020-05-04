c043c078958e413d9566505038ef153671a2ef5b
Yuhei Nishi <abc-gtgtg.21q.abds@ezweb.ne.jp>
2020-05-05 04:27:41 +0900

    グループ機能　追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,32 @@
 class TweetsController < ApplicationController
+  before_action :set_group
+
   def index
+    @tweet = Tweet.new
+    @tweets = @group.tweets.includes(:user)
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = @group.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to group_tweets_path(@group), notice: 'メッセージが送信されました'
+    else
+      @tweets = @group.tweets.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
